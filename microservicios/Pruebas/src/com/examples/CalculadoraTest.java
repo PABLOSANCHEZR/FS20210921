4564c88ba800680400431cf377a23471ffb4d203
PABLOSANCHEZR <PABLO.SANCHEZR@VIEWNEXT.COM>
2021-10-19 10:18:42 +0200

    pruebas eclipse

--- microservicios/Pruebas/src/com/examples/CalculadoraTest.java
+++ microservicios/Pruebas/src/com/examples/CalculadoraTest.java
@@ -1,21 +1,28 @@
 package com.examples;
 
+import static org.junit.Assume.assumeTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class CalculadoraTest {
-	
 	Calculadora calc;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
-		
 	}
 
 	@AfterAll
@@ -23,29 +30,55 @@ class CalculadoraTest {
 	}
 
 	@BeforeEach
-	void setUp() throws Exception {	
+	void setUp() throws Exception {
 		calc = new Calculadora();
-
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
 	}
 
-	@Test
-	void testSuma() {
-		assertEquals(4, calc.suma(2,2));
+	@ParameterizedTest(name = "Suma {index} => {0} + {1} = {2}")
+	@CsvSource({ "2,2,4", "0,0,0", "1,-1,0" })
+	void testSuma(double a, double b, double rslt) {
+		assumeTrue(b >= 0);
+		assertEquals(rslt, calc.suma(a, b));
 	}
 
 	@Test
-	void testDivide_Double_Double() {
-		assertEquals(0.5, calc.divide(1.0, 2));
+	@Tag("smoke")
+	@Tag("lenta")
+	void testHumo() {
+		assertEquals(3, calc.suma(1, 2));
 	}
+	@Nested
+	@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+	class Divisiones {
+		@RepeatedTest(value = 5, name = "{displayName} {currentRepetition}/{totalRepetitions}")
+		void test_Divide_Double_Double() {
+//			assertEquals(0.5, calc.divide(1.0, 2));
+//			//assertThrows(Exception.class, () -> calc.divide(1.0, 0));
+//			assertEquals(Double.POSITIVE_INFINITY, calc.divide(1.0, 0));
+////			var d = 1 / 0;
+			assertAll("Divisiones enteras", 
+				() -> assertEquals(0.5, calc.divide(1.0, 2), "la real"),
+//				() -> assertEquals(0.5, calc.divide(1, 2), "La entera"),
+				() -> assertEquals(Double.POSITIVE_INFINITY, calc.divide(1.0, 0))
+			);
+		}
 
-	@Test
-	@DisplayName("Division_entera")
-	void testDivideIntInt() {
-		assertEquals(0, calc.divide(1, 2));
+		@Test
+		@DisplayName("Division entera")
+		@Disabled
+		void testDivideIntInt() {
+			assertEquals(0, calc.divide(1, 2));
+//			try {
+//				assertEquals(0, calc.divide(1, 0), "la primera");
+//			} catch (Exception e) {
+//				fail("Excepcion no controlada");
+//			}
+//			assertEquals(0, calc.divide(0, 0), "la segunda");
+			assertThrows(Exception.class, () -> calc.divide(1, 0));
+		}
 	}
-
 }
